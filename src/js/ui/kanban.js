b9c725a70fa9602d7f834736fd818d79c7ff0f08
Hedellen Fernandes <hedellenf@gmail.com>
2020-02-05 22:51:03 -0300

    remove Jkanban e adiciona dracula como drag and drop padrao

--- src/js/ui/kanban.js
+++ src/js/ui/kanban.js
@@ -1,75 +1,8 @@
-/* eslint-disable no-undef */
-export default async function Kanban() {
+import dragula from 'dragula'
 
-	const tasksKanban = document.getElementById('tasksKanban')
-
-	let tasksKanbanProps = await {
-		width: tasksKanban.offsetWidth
-	}
-
-	let kanban = new jKanban({
-		element: '#tasksKanban',
-		widthBoard: (tasksKanbanProps.width / 3) - 18,
-		boards: [
-			{
-				id: '_todo',
-				title: 'Nova tarefa',
-				class: 'info,good',
-				dragTo: ['_working'],
-				item: [
-					{
-						id: '_test_delete',
-						title: 'Try drag this (Look the console)',
-						username: 'hedellen',
-						drag: function (el) {
-							console.log('START DRAG: ' + el.dataset.eid)
-						},
-						dragend: function (el) {
-							console.log('END DRAG: ' + el.dataset.eid)
-						},
-						drop: function (el) {
-							console.log('DROPPED: ' + el.dataset.eid)
-						}
-					},
-					{
-						title: 'Try Click This!',
-						click: function () {
-							alert('click')
-						},
-						username: "username1",
-						class: ['peppe', 'bello']
-					}
-				]
-			},
-			{
-				id: '_working',
-				title: 'Em andamento',
-				class: 'warning',
-				item: [
-					{
-						title: 'Do Something!'
-					},
-					{
-						title: 'Run?'
-					}
-				]
-			},
-			{
-				id: '_done',
-				title: 'Concluído',
-				class: 'success',
-				dragTo: ['_working'],
-				item: [
-					{
-						title: 'All right'
-					},
-					{
-						title: 'Ok!'
-					}
-				]
-			}
-		]
-	})
-
-	return kanban
+export default function Kanban() {
+	const boxArray = document.getElementsByClassName('board')
+	const boxes = Array.prototype.slice.call(boxArray)
+	
+	dragula({ containers: boxes })
 }
