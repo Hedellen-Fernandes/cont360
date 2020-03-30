ca03229265589fc712eb54f3d5edc0cc2c4e04f5
Hedellen Fernandes <hedellenf@gmail.com>
2020-03-29 21:18:03 -0300

    RH finalizado

--- src/js/index.js
+++ src/js/index.js
@@ -4,11 +4,13 @@ import '../../node_modules/jkanban/dist/jkanban.js'
 
 import Nav from './ui/nav'
 import Kanban from './ui/kanban'
+import Datepicker from './ui/date-picker'
 
 function Initialize() {
 	const Ui = () => {
 		Nav()
 		Kanban()
+		Datepicker()
 	}
 
 	Ui()
---
+++ src/js/ui/date-picker.js
@@ -0,0 +1,10 @@
+export default function Datepicker() {
+	const inputData = document.querySelectorAll('.date-picker')
+
+	Array.from(inputData).map(item => {
+		item.addEventListener('focus', event => {
+			const target = event.target
+			target.setAttribute('value', new Date().toISOString().substr(0, 10))
+		})
+	})
+}
