213bdd6855941a15cb5a356a3420f75a3c2ccb38
Hedellen Fernandes <hedellenf@gmail.com>
2020-05-17 20:18:04 -0300

    Alterações datepicker e select

--- src/js/ui/label-material.js
+++ src/js/ui/label-material.js
@@ -1,9 +1,30 @@
 import createElement from '../components/createElement'
 
 export default function labelMaterial() {
-	const input = document.querySelectorAll('.form-control')
+	const inputs = document.querySelectorAll('.form-control')
+	const selects = document.querySelectorAll('select')
 
-	Array.from(input).map(item => {
+	materialInput(inputs)
+	materialSelect(selects)
+}
+
+function materialSelect(list) {
+	Array.from(list).map(item => {
+		const labelValue = item.options[0].value
+		const labelInput = createElement('label', labelValue)
+		const parentNode = item.parentNode
+
+		if (item.classList.contains('select-dark'))
+			labelInput.style.color = '#8ad6fd'
+
+		item.options[0] = null
+		parentNode.classList.add('input-container', 'is-active')
+		parentNode.insertBefore(labelInput, item)
+	})
+}
+
+function materialInput(list) {
+	Array.from(list).map(item => {
 		if (item.type != 'search') {
 			const placeholderValue = item.placeholder
 			const labelInput = createElement('label', placeholderValue)
